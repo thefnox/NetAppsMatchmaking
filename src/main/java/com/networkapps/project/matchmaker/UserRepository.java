16465928864803de08d7b79e169da904df4e17dd
fernando <fvalarinon@gmail.com>
2019-01-25 20:01:29 +0100

    added db stuff

--- src/main/java/com/networkapps/project/matchmaker/UserRepository.java
+++ src/main/java/com/networkapps/project/matchmaker/UserRepository.java
@@ -5,12 +5,15 @@
  */
 package com.networkapps.project.matchmaker;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 /**
  *
  * @author kamai
  */
 public interface UserRepository extends JpaRepository<User, String> {
-    
+    @Query("SELECT u FROM User u WHERE u.id = :name")
+    public List<User> findByName(@Param("name") String name);
 }
