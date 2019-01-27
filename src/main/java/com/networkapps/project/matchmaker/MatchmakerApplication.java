dd4cf76b9c2458000d48383c36c6abd83dcd5103
fnox <thefnox@gmail.com>
2019-01-27 15:18:24 +0100

    Added server side push.

--- src/main/java/com/networkapps/project/matchmaker/MatchmakerApplication.java
+++ src/main/java/com/networkapps/project/matchmaker/MatchmakerApplication.java
@@ -1,11 +1,13 @@
 package com.networkapps.project.matchmaker;
 
+import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MatchmakerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MatchmakerApplication.class, args);
 	}
