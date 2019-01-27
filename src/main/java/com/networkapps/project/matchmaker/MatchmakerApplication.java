4fabdeeec98bfdbcea2112b2548ea3f5343e0a34
Martin Huynh <m.huynh@hotmail.co.uk>
2019-01-27 19:45:37 +0100

    Added global CORS
    CORS added

--- src/main/java/com/networkapps/project/matchmaker/MatchmakerApplication.java
+++ src/main/java/com/networkapps/project/matchmaker/MatchmakerApplication.java
@@ -1,16 +1,21 @@
 package com.networkapps.project.matchmaker;
 
-import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
-public class MatchmakerApplication {
+public class MatchmakerApplication implements WebMvcConfigurer{
 	public static void main(String[] args) {
 		SpringApplication.run(MatchmakerApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*");
+	}
 }
 
